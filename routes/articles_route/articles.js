2d6de749a424c74e206763c2f0ee4e42b2aee754
cosmic <kingsleyfrancis42@gmail.com>
2022-07-23 17:55:17 -0400

    myy project

---
+++ routes/articles_route/articles.js
@@ -0,0 +1,16 @@
+const router=require("express").Router()
+const express=require("express")
+const middlewares = require("./articles_data")
+router.route("/article/emacs_lspe")
+    .post((req,res)=>{
+	middlewares.create_article()
+    })
+    .get((req,res)=>{
+	middlewares.fetch_article()
+    })
+    .delete(()=>{
+	middlewares.delete_article
+    })
+    .patch(()=>{
+	middlewares.update_article()
+    })
---
+++ routes/articles_route/articles_data.js
@@ -0,0 +1,13 @@
+const create_article = (req,res)=>{
+    res.send("hello world");
+}
+const delete_article = (req,res)=>{
+    res.send("hello world");
+}
+const update_article = (req,res)=>{
+    res.send("hello world");
+}
+const fetch_article = (req,res)=>{
+    res.send("hello world");
+}
+module.exports ={create_article,delete_article,fetch_article,update_article};
