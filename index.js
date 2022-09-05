d7b813ceed64542f541246911f5a136ce1b33ad3
cosmic <kingsleyfrancis42@gmail.com>
2022-09-05 02:58:59 -0400

    cloudinary

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express=require('express')
 const connectdb= require('./routes/mongoose/connectdb')
 const articles = require('./routes/articles_route/articles')
 const register = require('./routes/user_route/user')
+const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT
 const app=express()
 connectdb(process.env.DB_URI)
 app.use(express.json())
+app.use(cors({
+    origin:"https://cosmic-school-api-node.vercel.app"
+}))
 app.use('/',articles)
 app.use('/',register)
 
