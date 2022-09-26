4f1ce57b726f1f6fd91f6f8682eb0b4178dd9c47
cosmic <kingsleyfrancis42@gmail.com>
2022-09-25 22:52:39 -0400

    port try

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const articles = require('./articles_route/articles')
 const register = require('./user_route/user')
 const cors = require('cors')
 require('dotenv').config()
-const port = 8080;
 const app=express()
 connectdb(process.env.DB_URI)
 app.use(express.json())
