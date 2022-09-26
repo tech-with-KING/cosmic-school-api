0da65a51d0572f85fe6376ef5224524d98bbb9e6
cosmic <kingsleyfrancis42@gmail.com>
2022-09-25 22:01:14 -0400

    hyp

--- mongoose/connectdb.js
+++ mongoose/connectdb.js
@@ -4,7 +4,6 @@ connectdb=async(url)=>{
     try {
         const connect=await mongoose.connect('mongodb+srv://kingsley:kingsley2018@cluster0.orzrj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{useNewUrlParser:true},()=>{ 
             console.log('connected to db')
-	    console.log(typeof(url))
         }
         )
     } catch (error) {
